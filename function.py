6f4f8ea595757b1a345dc0e756465d34ae067cb4
Yuchuan Xu <135388391+yuchuanxu15@users.noreply.github.com>
2023-07-12 16:53:42 +0100

    Create function.py

---
+++ function.py
@@ -0,0 +1,49 @@
+
+def extract_data(element, dataframe):
+
+    name = element.find('h2').text
+    country = element.find('span').text
+
+    year_and_funding = element.find(class_='flex flex-col gap-0')
+    website = element.find("a").text
+    description = element.find("p").text
+
+    year_and_funding = year_and_funding.text
+
+    # Use regular expressions to extract the founding year
+    founding_year_match = re.search(r"Founding Year: (\d+)", year_and_funding)
+
+    if founding_year_match:
+        founding_year = founding_year_match.group(1)
+        # Build the second part string
+        year = founding_year
+    else:
+        year = ""
+
+    # Use regular expressions to extract the total funding
+    total_funding_match = re.search(r"Total Funding: (\$.+)", year_and_funding)
+
+    if total_funding_match:
+        total_funding = total_funding_match.group(1)
+        # Build the first part string
+        funding = total_funding
+    else:
+        funding = ""
+
+    # Create a dictionary with the new data
+    new_data = {
+        "Name": [name],
+        "Country": [country],
+        "Year": [year],
+        "Funding": [funding],
+        "Website": [website],
+        "Description": [description]
+    }
+
+    # Create a new dataframe from the new data
+    new_dataframe = pd.DataFrame(new_data)
+
+    # Append the new dataframe to the existing dataframe
+    data = pd.concat([dataframe, new_dataframe], ignore_index=True)
+
+    return data
