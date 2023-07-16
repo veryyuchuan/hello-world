e2a0bde7c74f2686462c33737dd6185bb9507b10
Yuchuan Xu <135388391+yuchuanxu15@users.noreply.github.com>
2023-07-16 11:33:46 +0100

    Update data_wrangling.py

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-# checking duplicate records
+# check duplicate records
 def check_duplicates(data_file):
     # Load the dataset into a pandas DataFrame
     data = pd.read_csv(data_file)
@@ -15,3 +15,17 @@ def check_duplicates(data_file):
         print(duplicate_records)
     else:
         print("No duplicate records found.")
+
+# remove duplicate records
+def remove_duplicates(data_file):
+    # Load the dataset into a pandas DataFrame
+    data = pd.read_csv(data_file)
+
+    # Remove duplicate records
+    data = data.drop_duplicates()
+
+    # Save the cleaned data to a new file
+    cleaned_file = 'cleaned_' + data_file
+    data.to_csv(cleaned_file, index=False)
+
+    print("Duplicate records removed. Cleaned data saved to", cleaned_file)
